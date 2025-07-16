6f74d826c4a0d58baa882ceadfeb73fe2e0fb5c5
Chidiebere <chidieberemok@gmail.com>
2025-07-16 04:33:36 -0400

    Add files via upload

---
+++ Formatting CVD Data.sql
@@ -0,0 +1,6 @@
+create database cardiovasculardb;
+use cardiovasculardb;
+select * from cardio_train limit 10;
+select * from cardio_train where alco = 1 limit 5;
+select count(*) from cardio_train where alco = 1;
+
